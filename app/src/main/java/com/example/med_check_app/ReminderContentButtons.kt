f45d8a2a38f64769db815b6dd46b07befc822a9f
Parker <s204814@student.dtu.dk>
2023-06-08 10:34:55 +0200

    Firebase adding in progress

--- app/src/main/java/com/example/med_check_app/ReminderContentButtons.kt
+++ app/src/main/java/com/example/med_check_app/ReminderContentButtons.kt
@@ -5,30 +5,5 @@ import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
 
 @Composable
-fun ReminderContentButtons() {
-    val context = LocalContext.current
-
-    val positiveButtonClick = { dialog: DialogInterface, which: Int ->
-        Toast.makeText(
-            context,
-            stringResource(android.R.string.yes),
-            Toast.LENGTH_SHORT
-        ).show()
-    }
-    val negativeButtonClick = { dialog: DialogInterface, which: Int ->
-        Toast.makeText(
-            context,
-            stringResource(android.R.string.no),
-            Toast.LENGTH_SHORT
-        ).show()
-    }
-    val neutralButtonClick = { dialog: DialogInterface, which: Int ->
-        Toast.makeText(
-            context,
-            "Maybe",
-            Toast.LENGTH_SHORT
-        ).show()
-    }
-
-    // Rest of your code
+fun ReminderContentButtons()
 }
