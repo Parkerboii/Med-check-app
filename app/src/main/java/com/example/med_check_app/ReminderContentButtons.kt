d697102dfb709b77201af72f5af58732f0e1bac6
Marcus Andersen <marcusbuur1@hotmail.coom>
2023-06-10 14:16:58 +0200

    Påmindelser med tid

--- app/src/main/java/com/example/med_check_app/ReminderContentButtons.kt
+++ app/src/main/java/com/example/med_check_app/ReminderContentButtons.kt
@@ -1,3 +1,6 @@
+import android.annotation.SuppressLint
+import android.app.TimePickerDialog
+import android.content.Context
 import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
@@ -12,16 +15,20 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.DialogProperties
-import kotlinx.coroutines.Job
-import kotlinx.coroutines.delay
+import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.launch
+import java.util.*
 
+@SuppressLint("UnrememberedMutableState")
 @Composable
 fun ReminderContentButtons() {
     val tasks = remember { mutableStateListOf("Paracetamol", "Morfin", "Paracetamol") }
     var selectedItem by remember { mutableStateOf("") }
+    var isTimePickerVisible by remember { mutableStateOf(false) }
+    var selectedTime by remember { mutableStateOf("") }
 
     Column(modifier = Modifier.padding(16.dp)) {
         tasks.forEachIndexed { index, task ->
@@ -39,6 +46,7 @@ fun ReminderContentButtons() {
                     .width(335.dp)
                     .clickable {
                         selectedItem = task
+                        isTimePickerVisible = true
                     }
             ) {
                 Row(verticalAlignment = Alignment.CenterVertically) {
@@ -53,18 +61,32 @@ fun ReminderContentButtons() {
         }
     }
 
-    if (selectedItem.isNotEmpty()) {
-        AlertDialog(
-            onDismissRequest = { selectedItem = "" },
-            title = { Text(text = "Selected Item") },
-            text = { Text(text = "You clicked on: $selectedItem") },
-            confirmButton = {
-                Button(onClick = { selectedItem = "" }) {
-                    Text(text = "OK")
-                }
-            },
-            properties = DialogProperties(dismissOnClickOutside = false)
-        )
+    val context = LocalContext.current
+
+    if (isTimePickerVisible) {
+        showTimePickerDialog(context) { time ->
+            selectedTime = time
+            isTimePickerVisible = false
+        }
     }
 }
 
+@SuppressLint("NewApi")
+fun showTimePickerDialog(context: Context, onTimeSelected: (String) -> Unit) {
+    val calendar = Calendar.getInstance()
+    val hour = calendar.get(Calendar.HOUR_OF_DAY)
+    val minute = calendar.get(Calendar.MINUTE)
+
+    val timePickerDialog = TimePickerDialog(
+        context,
+        { _, selectedHour, selectedMinute ->
+            val time = String.format("%02d:%02d", selectedHour, selectedMinute)
+            onTimeSelected(time)
+        },
+        hour,
+        minute,
+        true
+    )
+
+    timePickerDialog.show()
+}
