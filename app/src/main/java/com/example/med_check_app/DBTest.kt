e2b00cb17afc8b315acc609a75cb150321ae620b
Parker <s204814@student.dtu.dk>
2023-06-12 13:30:32 +0200

    Help me stepbro, I'm stuck

---
+++ app/src/main/java/com/example/med_check_app/DBTest.kt
@@ -0,0 +1,18 @@
+package com.example.med_check_app
+
+import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
+import androidx.compose.runtime.Composable
+import com.google.firebase.firestore.FirebaseFirestore
+
+
+lateinit var db: FirebaseFirestore
+@Composable
+fun dbTest(){}
+
+
+
+
+
+
