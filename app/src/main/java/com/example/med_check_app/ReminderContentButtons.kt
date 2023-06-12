e570099788a4f07b5746f015070429c2193212c7
Marcus Andersen <marcusbuur1@hotmail.coom>
2023-06-12 17:58:57 +0200

    Påmindelser scrollable

--- app/src/main/java/com/example/med_check_app/ReminderContentButtons.kt
+++ app/src/main/java/com/example/med_check_app/ReminderContentButtons.kt
@@ -3,19 +3,19 @@ import android.app.AlertDialog
 import android.content.Context
 import android.widget.ArrayAdapter
 import android.widget.Spinner
+import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
-import androidx.compose.material.Button
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.Checkbox
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
 import com.example.med_check_app.showTimePickerDialog
@@ -38,38 +38,45 @@ fun ReminderContentButtons() {
         "Hver 4 dag", "Hver 5 dag", "Hver 6 dag", "Hver 7 dag"
     )
 
-    Column(modifier = Modifier.padding(16.dp)) {
-        tasks.forEachIndexed { index, task ->
-            var isChecked by remember { mutableStateOf(false) }
+    val context = LocalContext.current
 
-            Column(
-                modifier = Modifier
-                    .padding(bottom = 8.dp)
-                    .border(
-                        width = 1.dp,
-                        color = MaterialTheme.colors.onSurface.copy(alpha = 0.1f),
-                        shape = MaterialTheme.shapes.medium
-                    )
-                    .padding(8.dp)
-                    .width(335.dp)
-                    .clickable {
-                        selectedItem = task
-                        isAlertDialogVisible = true
+    Column(modifier = Modifier.padding(16.dp)) {
+        LazyColumn {
+            items(tasks.size) { index ->
+                val task = tasks[index]
+                var isChecked by remember { mutableStateOf(false) }
+
+                Column(
+                    modifier = Modifier
+                        .padding(bottom = 8.dp)
+                        .background(
+                            color = Color.White,
+                            shape = MaterialTheme.shapes.medium
+                        )
+                        .clickable {
+                            selectedItem = task
+                            isAlertDialogVisible = true
+                        }
+                        .fillMaxWidth()
+                        .padding(8.dp)
+                        .border(
+                            width = 1.dp,
+                            color = MaterialTheme.colors.onSurface.copy(alpha = 0.1f),
+                            shape = MaterialTheme.shapes.medium
+                        )
+                ) {
+                    Row(verticalAlignment = Alignment.CenterVertically) {
+                        Checkbox(
+                            checked = isChecked,
+                            onCheckedChange = { isChecked = it },
+                            modifier = Modifier.padding(end = 8.dp)
+                        )
+                        Text(text = task)
                     }
-            ) {
-                Row(verticalAlignment = Alignment.CenterVertically) {
-                    Checkbox(
-                        checked = isChecked,
-                        onCheckedChange = { isChecked = it },
-                        modifier = Modifier.padding(end = 8.dp)
-                    )
-                    Text(text = task)
                 }
             }
         }
     }
-
-    val context = LocalContext.current
 
     if (isAlertDialogVisible) {
         val spinner = createSpinner(context, frequencyOptions, selectedFrequency)
