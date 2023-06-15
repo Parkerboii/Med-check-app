9bdaff3ede22873b43ac69104196a0a824565841
niels-christianbeyerhansen <niels.beyer71@gmail.com>
2023-06-15 10:12:03 +0200

    Login til andersen

---
+++ app/src/main/java/com/example/med_check_app/Login.kt
@@ -0,0 +1,104 @@
+package com.example.med_check_app
+
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.*
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.navigation.NavController
+
+/*@Composable
+fun LoginPage(navController: NavController) {
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center,
+        modifier = Modifier.fillMaxSize()
+    ) {
+        Text(text = "")
+    }
+    Button(
+        colors = ButtonDefaults.buttonColors(
+            backgroundColor = androidx.compose.ui.graphics.Color.Gray,
+            contentColor = androidx.compose.ui.graphics.Color.Black),
+        modifier = Modifier
+            .width(300.dp)
+            .padding(20.dp),
+        onClick = { navController.navigate((Screen.MainScreen.route)) }) {
+        Text("Login", fontSize = 30.sp)
+    }
+}
+ */
+@Composable
+fun LoginPage(navController: NavController) {
+    val username = remember { mutableStateOf("") }
+    val password = remember { mutableStateOf("") }
+    val loginError = remember { mutableStateOf(false) } // Track login error
+
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center,
+        modifier = Modifier.fillMaxSize()
+    ) {
+        Text(text = "Login", fontSize = 35.sp, fontWeight = FontWeight.Bold)
+        Spacer(modifier = Modifier.height(16.dp))
+        TextField(
+            value = username.value,
+            onValueChange = { username.value = it },
+            label = { Text(text = "Username") },
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(horizontal = 20.dp)
+        )
+        Spacer(modifier = Modifier.height(16.dp))
+        TextField(
+            value = password.value,
+            onValueChange = { password.value = it },
+            label = { Text(text = "Password") },
+            visualTransformation = PasswordVisualTransformation(),
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(horizontal = 20.dp)
+        )
+        if (loginError.value) {
+            Text(
+                text = "Forkert Brugernavn eller Password, prøv igen",
+                color = Color.Red,
+                modifier = Modifier.padding(vertical = 8.dp)
+            )
+        }
+        Spacer(modifier = Modifier.height(16.dp))
+        Button(
+            onClick = {
+                val loggedIn = performLogin(username.value, password.value)
+                if (loggedIn) {
+                    navController.navigate(Screen.MainScreen.route)
+                } else {
+                    loginError.value = true // Set login error to true
+                }
+            },
+            colors = ButtonDefaults.buttonColors(
+                backgroundColor = Color.Blue,
+                contentColor = Color.White
+            ),
+            modifier = Modifier
+                .width(300.dp)
+                .padding(20.dp)
+        ) {
+            Text("Login", fontSize = 30.sp)
+        }
+    }
+}
+
+fun performLogin(username: String, password: String): Boolean {
+    return username == "admin" && password == "password"
+
+}
+
+
